f03c4da244375601530f75b34736196b9e54888d
Loris <45939615+lchastanet@users.noreply.github.com>
2022-06-13 15:26:39 +0200

    add reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react"
+import TodoCard from "./components/TodoCard"
+import TodoForm from "./components/TodoForm"
+
+import { todoReducer, init } from "./reducers/todoReducer"
 
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, [], init)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        {todos.map((todo) => (
+          <TodoCard
+            key={Math.floor(Math.random() * 1000)}
+            todo={todo}
+            dispatch={dispatch}
+          />
+        ))}
+      </div>
+      <TodoForm dispatch={dispatch} />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function TodoForm({ dispatch }) {
+  const [content, setContent] = useState("")
+
+  return (
+    <>
+      <div>todoForm</div>
+      <input
+        onChange={(e) => {
+          setContent(e.target.value)
+        }}
+        type="text"
+        name="textIpnut"
+        id="textIpnut"
+      />
+      <button
+        onClick={() => {
+          dispatch({
+            type: "ADD_TODO",
+            payload: { content, status: false },
+          })
+        }}
+      >
+        Ajouter
+      </button>
+    </>
+  )
+}
+
+export default TodoForm
